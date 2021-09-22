62fd8e2e972a1f0da457d1351ad31f656b3e0ffc
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-09-22 19:17:48 +0530

    Add files via upload

---
+++ npm proxy config.bat
@@ -0,0 +1,13 @@
+@echo off
+echo starting the script.....
+echo setting up proxies
+npm config set proxy http://172.16.0.3:3128
+
+
+npm config set https-proxy http://172.16.0.3:3128
+
+echo installing create-react-app
+npm install -g create-react-app
+echo the script has ended execution
+pause
+pause
